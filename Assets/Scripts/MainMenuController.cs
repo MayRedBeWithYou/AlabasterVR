15432c59a9d6cfaeeefb1da6598204f583fe3995
Dominik Sellin <mayredbewithyou@gmail.com>
2020-10-13 13:17:35 +0200

    Improved control mapping, added test UI controls for main menu

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    public Button NewFileButton;
+
+    public Button SaveButton;
+
+    public Button ImportButton;
+
+    public Button OptionsButton;
+
+    public Button ExitButton;
+}
--- Assets/Scripts/ToolController.cs
+++ Assets/Scripts/ToolController.cs
@@ -1,15 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class ToolController : MonoBehaviour
 {
-    public Transform rightControllerTransform;
+    public GameObject rightController;
+    public GameObject leftController;
 
-    public Transform leftControllerTransform;
+    public Transform cameraTransform;
+    public Transform leftPointer;
+    public Transform rightPointer;
+
+    public ButtonHandler MainMenuButtonHandler;
+    public ButtonHandler LeftPointerButtonHandler;
+    public ButtonHandler RightPointerButtonHandler;
+
+    public GameObject MainMenuPrefab;
 
     private static ToolController _instance;
     public static ToolController Instance => _instance;
+
+    private GameObject _activeMainMenu = null;
 
     private void Awake()
     {
@@ -20,6 +34,38 @@ public class ToolController : MonoBehaviour
         }
         _instance = this;
         DontDestroyOnLoad(this.gameObject);
+        MainMenuButtonHandler.OnButtonDown += ShowMainMenu;
+
+        LeftPointerButtonHandler.OnButtonDown += (controller) => ToggleLineVisual(controller, true);
+        LeftPointerButtonHandler.OnButtonUp += (controller) => ToggleLineVisual(controller, false);
+
+        RightPointerButtonHandler.OnButtonDown += (controller) => ToggleLineVisual(controller, true);
+        RightPointerButtonHandler.OnButtonUp += (controller) => ToggleLineVisual(controller, false);
     }
 
+    private void ToggleLineVisual(XRController controller, bool value)
+    {
+        controller.GetComponentInParent<XRRayInteractor>().enabled = value;
+        controller.GetComponentInParent<XRInteractorLineVisual>().enabled = value;
+    }
+
+    private void ShowMainMenu(XRController controller)
+    {
+        if (_activeMainMenu)
+        {
+            CloseMainMenu();
+        }
+        else
+        {
+            _activeMainMenu = Instantiate(MainMenuPrefab, leftPointer.position, Quaternion.LookRotation(leftController.transform.position - cameraTransform.position, Vector3.up));
+            MainMenuController mainMenu = _activeMainMenu.GetComponent<MainMenuController>();
+            mainMenu.ExitButton.onClick.AddListener(CloseMainMenu);
+        }
+    }
+
+    private void CloseMainMenu()
+    {
+        Destroy(_activeMainMenu);
+        _activeMainMenu = null;
+    }
 }
