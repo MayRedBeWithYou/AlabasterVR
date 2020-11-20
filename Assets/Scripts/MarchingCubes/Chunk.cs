fb9795913a5a9681ed5472540f6467e4f9e7bd10
Dominik Sellin <mayredbewithyou@gmail.com>
2020-11-20 19:50:14 +0100

    - Added keyboard mockup - Added MaterialTool working prototype - Added LayerManager - Added basic layer support - Added chunk generation

---
+++ Assets/Scripts/MarchingCubes/Chunk.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour
+{
+    public int size;
+
+    public float boundSize;
+
+    public Vector3Int coord = Vector3Int.zero;
+
+    public Vector3 center
+    {
+        get
+        {
+            return (Vector3)coord * boundSize + Vector3.one * boundSize / 2;
+        }
+    }
+
+    public Vector3 offset
+    {
+        get
+        {
+            return center - Vector3.one * boundSize / 2f;
+        }
+    }
+
+    [HideInInspector]
+    public Mesh mesh;
+
+    private MeshFilter _filter;
+    private MeshRenderer _renderer;
+    private MeshCollider _collider;
+
+    [HideInInspector]
+    public VoxelData[] voxelArray;
+
+
+    public int GetVoxelIndex(int x, int y, int z)
+    {
+        return x + y * size + z * size * size;
+    }    
+
+    public void Awake()
+    {
+        _filter = GetComponent<MeshFilter>();
+        _renderer = GetComponent<MeshRenderer>();
+        _collider = GetComponent<MeshCollider>();
+
+        mesh = new Mesh();
+        mesh.MarkDynamic();
+        _filter.sharedMesh = mesh;
+        mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+    }
+
+    public void GenerateCollider()
+    {
+        _collider.enabled = false;
+        _collider.enabled = true;
+    }
+}
+
+public struct VoxelData
+{
+    public Vector3 position;
+    public float value;
+}
---
+++ Assets/Scripts/MeshGenerator.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshGenerator : MonoBehaviour
+{
+    private static MeshGenerator _instance;
+
+    public static MeshGenerator Instance => _instance;
+
+    public int threadGroupSize = 8;
+
+    public ComputeShader shader;
+
+    public bool displayDebug;
+
+    // Buffers
+    ComputeBuffer triangleBuffer;
+    ComputeBuffer pointsBuffer;
+    ComputeBuffer triCountBuffer;
+
+    public void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        _instance = this;
+    }
+
+    public void UpdateAllActiveChunks()
+    {
+        foreach (Chunk chunk in LayerManager.Instance.activeChunks)
+            UpdateChunkMesh(chunk);
+    }
+
+    public void UpdateChunkMesh(Chunk chunk)
+    {
+        int numVoxelsPerAxis = chunk.size - 1;
+        int numThreadsPerAxis = Mathf.CeilToInt(numVoxelsPerAxis / (float)threadGroupSize);
+
+        CreateBuffers();
+
+        pointsBuffer.SetData(chunk.voxelArray);
+        triangleBuffer.SetCounterValue(0);
+        shader.SetBuffer(0, "points", pointsBuffer);
+        shader.SetBuffer(0, "triangles", triangleBuffer);
+        shader.SetInt("numPointsPerAxis", chunk.size);
+        shader.SetFloat("isoLevel", 0);
+
+        shader.Dispatch(0, numThreadsPerAxis, numThreadsPerAxis, numThreadsPerAxis);
+
+        // Get number of triangles in the triangle buffer
+        ComputeBuffer.CopyCount(triangleBuffer, triCountBuffer, 0);
+        int[] triCountArray = { 0 };
+        triCountBuffer.GetData(triCountArray);
+        int numTris = triCountArray[0];
+
+        // Get triangle data from shader
+        Triangle[] tris = new Triangle[numTris];
+        triangleBuffer.GetData(tris, 0, 0, numTris);
+
+        Mesh mesh = chunk.mesh;
+        mesh.Clear();
+
+        var vertices = new Vector3[numTris * 3];
+        var meshTriangles = new int[numTris * 3];
+
+        for (int i = 0; i < numTris; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                meshTriangles[i * 3 + j] = i * 3 + j;
+                vertices[i * 3 + j] = tris[i][j];
+            }
+        }
+        mesh.vertices = vertices;
+        mesh.triangles = meshTriangles;
+
+        mesh.RecalculateNormals();
+        chunk.GenerateCollider();
+
+        ReleaseBuffers();
+
+        if (displayDebug)
+            Debug.Log($"Generated mesh for chunk ({chunk.coord.x},{chunk.coord.y},{chunk.coord.z}) with {mesh.triangles.Length} triangles and {mesh.vertices.Length} vertices.");
+    }
+
+    void CreateBuffers()
+    {
+        int chunkSize = LayerManager.Instance.chunkSize;
+        int numPoints = chunkSize * chunkSize * chunkSize;
+        int numVoxelsPerAxis = chunkSize - 1;
+        int numVoxels = numVoxelsPerAxis * numVoxelsPerAxis * numVoxelsPerAxis;
+        int maxTriangleCount = numVoxels * 5;
+
+        triangleBuffer = new ComputeBuffer(maxTriangleCount, sizeof(float) * 3 * 3, ComputeBufferType.Append);
+        pointsBuffer = new ComputeBuffer(numPoints, sizeof(float) * 4);
+        triCountBuffer = new ComputeBuffer(1, sizeof(int), ComputeBufferType.Raw);
+
+    }
+
+    private void OnDestroy()
+    {
+        ReleaseBuffers();
+    }
+
+    void ReleaseBuffers()
+    {
+        if (triangleBuffer != null)
+        {
+            triangleBuffer.Release();
+            pointsBuffer.Release();
+            triCountBuffer.Release();
+        }
+    }
+
+    struct Triangle
+    {
+        public Vector3 a;
+        public Vector3 b;
+        public Vector3 c;
+
+        public Vector3 this[int i]
+        {
+            get
+            {
+                switch (i)
+                {
+                    case 0:
+                        return a;
+                    case 1:
+                        return b;
+                    default:
+                        return c;
+                }
+            }
+        }
+    }
+}
