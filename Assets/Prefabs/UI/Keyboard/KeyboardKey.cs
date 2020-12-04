02f8d248c5d196b0e42eda3d75aba2d2bb1ae75d
GrabowskaElzbieta <grabowska.e.b@gmail.com>
2020-12-04 19:11:28 +0100

    Added keyboard support

---
+++ Assets/Prefabs/UI/Keyboard/KeyboardKey.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyboardKey : MonoBehaviour
+{
+    UnityEngine.UI.Button button;
+    UnityEngine.UI.InputField inputField;
+    public char Primary;
+    public char Secondary;
+    public char AltPrimary;
+    public char AltSecondary;
+    public bool IsLetter{get{return Primary>='a'&&Primary<='z';}}    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button=this.gameObject.GetComponent<Button>();
+        button.onClick.AddListener(OnClick);
+        inputField=button.gameObject.transform.parent.parent.parent.GetChild(0).GetChild(0).gameObject.GetComponent<InputField>();
+        if(Primary=='_'&&Secondary=='_') Primary=Secondary=AltPrimary=AltSecondary=' ';
+    }
+    public void Change2Primary()
+    {
+        button.GetComponentInChildren<Text>().text=Primary.ToString();
+    }
+    public void Change2Secondary()
+    {
+        button.GetComponentInChildren<Text>().text=Secondary.ToString();
+    }
+    public void Change2AltPrimary()
+    {
+        button.GetComponentInChildren<Text>().text=AltPrimary.ToString();
+    }
+    public void Change2AltSecondary()
+    {
+        button.GetComponentInChildren<Text>().text=AltSecondary.ToString();
+    }
+
+    public void OnClick()
+    {
+        if(inputField.text.Length==inputField.caretPosition)
+        {
+            inputField.text+=button.GetComponentInChildren<Text>().text;
+            inputField.caretPosition++;
+        }
+        else
+        {
+            string tmp=inputField.text.Substring(0,inputField.caretPosition)+button.GetComponentInChildren<Text>().text;
+            tmp+=inputField.text.Substring(inputField.caretPosition,inputField.text.Length-inputField.caretPosition);
+            inputField.text=tmp;
+            inputField.caretPosition++;
+        }
+        
+    }
+}
--- Assets/Scripts/ToolController.cs
+++ Assets/Scripts/ToolController.cs
@@ -27,6 +27,13 @@ public class ToolController : MonoBehaviour
     public GameObject ToolSelectionMenuPrefab;
     public Transform ToolSelectionMenuTransform;
 
+    [Header("Keyboard")]
+    public ButtonHandler KeyboardHandler;
+    public GameObject KeyboardPrefab;
+    private GameObject _activeKeyboard = null;
+
+
+
     private static ToolController _instance;
     public static ToolController Instance => _instance;
 
@@ -39,6 +46,9 @@ public class ToolController : MonoBehaviour
 
     private GameObject _activeMainMenu = null;
     private GameObject _activeToolSelectionMenu = null;
+    
+
+
 
     public Tool SelectedTool
     {
@@ -73,6 +83,7 @@ public class ToolController : MonoBehaviour
 
         ToolSelectionMenuButtonHandler.OnButtonDown += ShowToolSelectionMenu;
 
+        KeyboardHandler.OnButtonDown+=ShowKeyboard;
         foreach (Tool tool in ToolPrefabs)
         {
             Tools.Add(Instantiate(tool, transform));
@@ -114,4 +125,31 @@ public class ToolController : MonoBehaviour
         Destroy(_activeMainMenu);
         _activeMainMenu = null;
     }
+
+    private void ShowKeyboard(XRController controller)
+    {
+        Debug.Log("ShowKeyboard");
+        if (_activeKeyboard)
+        {
+            Destroy(_activeKeyboard);
+            _activeKeyboard = null;
+        }
+        else
+        {
+            Vector3 lookDirection = leftController.transform.position - cameraTransform.position;
+            lookDirection.y = 0;
+            _activeKeyboard = Instantiate(KeyboardPrefab, leftPointer.position + lookDirection.normalized * MainMenuDistance, Quaternion.LookRotation(lookDirection, Vector3.up));
+            var KeyboardPanelTransform=_activeKeyboard.transform.GetChild(0).GetChild(0).GetChild(1);
+            UnityEngine.UI.Button bCancel=KeyboardPanelTransform.GetChild(4).GetChild(5).gameObject.GetComponent<UnityEngine.UI.Button>();
+            UnityEngine.UI.Button bConfirm=KeyboardPanelTransform.GetChild(2).GetChild(12).gameObject.GetComponent<UnityEngine.UI.Button>();
+            
+            bCancel.onClick.AddListener(CloseKeyboard);
+            bConfirm.onClick.AddListener(CloseKeyboard);
+        }
+    }
+    private void CloseKeyboard()
+    {
+        Destroy(_activeKeyboard);
+        _activeKeyboard = null;
+    }    
 }
