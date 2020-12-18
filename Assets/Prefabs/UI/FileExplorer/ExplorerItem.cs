780c3d6637d90f81fcb2e9aceba1426197077609
Dominik Sellin <mayredbewithyou@gmail.com>
2020-12-18 18:51:13 +0100

    Added UI base for FileExplorer

---
+++ Assets/Prefabs/UI/FileExplorer/ExplorerItem.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExplorerItem : MonoBehaviour
+{
+    public enum ItemType
+    {
+        File,
+        Directory
+    }
+
+    private ItemType type;
+
+    public ItemType Type
+    {
+        get => type;
+        set
+        {
+            type = value;
+            image.sprite = type == ItemType.File ? fileIcon : directoryIcon;
+        }
+    }
+
+    public Image image;
+
+    public string path;
+
+    [SerializeField]
+    private Text text;
+
+    private string itemName;
+
+    public string ItemName
+    {
+        get => itemName;
+        set
+        {
+            itemName = value;
+            text.text = itemName;
+        }
+    }
+
+    public Sprite fileIcon;
+
+    public Sprite directoryIcon;
+
+    public void SelectItem()
+    {
+        switch (type)
+        {
+            case ItemType.File:
+
+                break;
+            case ItemType.Directory:
+
+                break;
+        }
+    }
+}
---
+++ Assets/Prefabs/UI/FileExplorer/FileExplorer.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FileExplorer : MonoBehaviour
+{
+    public enum FileMode
+    {
+        Open,
+        Save
+    }
+
+    public FileMode mode;
+
+    public string fileExtensions;
+
+    [SerializeField]
+    private Text headerText;
+
+    [SerializeField]
+    private GameObject entryHolder;
+
+    [SerializeField]
+    private Button acceptButton;
+
+    [SerializeField]
+    private Button cancelButton;
+
+    [SerializeField]
+    private GameObject itemPrefab;
+
+    public delegate void SelectPathCallback(string path);
+    public event SelectPathCallback OnAccepted;
+
+    public delegate void CancelCallback();
+    public event CancelCallback OnCancelled;
+
+    public delegate void CloseCallback();
+    public event CloseCallback OnClosing;
+
+    public string SelectedPath;
+
+    public void SetPath(string path)
+    {
+
+    }
+
+    public void Start()
+    {
+        acceptButton.onClick.AddListener(() => OnAccepted?.Invoke(SelectedPath));
+        cancelButton.onClick.AddListener(() => OnCancelled?.Invoke());
+    }
+
+    public void Close()
+    {
+        OnClosing?.Invoke();
+        gameObject.SetActive(false);
+        Destroy(gameObject);
+    }
+}
